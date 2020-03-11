dd06dc8ad3a552bbefe9a2b347959ad37e204fe2
Alla Voloshchuk <avoloshchuk@griddynamics.com>
2020-03-11 13:52:47 +0200

    Changes made according to Calculator pull request.

--- src/main/java/calculator/GetDataFromInputFile.java
+++ src/main/java/calculator/GetDataFromInputFile.java
@@ -1,8 +1,11 @@
 package calculator;
 
+import org.slf4j.*;
+
 import java.io.*;
 
 public class GetDataFromInputFile {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GetDataFromInputFile.class);
     private String filePath;
     private char operation;
     private double operand1;
@@ -13,30 +16,29 @@ public class GetDataFromInputFile {
     }
 
     public void readDataFromFile() {
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filePath)));
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(new File(filePath)));
+        ) {
             StringBuilder rawDataFromFile = new StringBuilder();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 rawDataFromFile.append(line + " ");
             }
-            bufferedReader.close();
             String[] dataFromFile = rawDataFromFile.toString().split(" ");
             try {
                 operation = dataFromFile[0].charAt(0);
             } catch (StringIndexOutOfBoundsException e) {
-                System.out.println("Operation " + operation + " doesn't exist \n" + e);
+                LOGGER.error("Operation " + operation + " doesn't exist \n" + e);
             }
             try {
                 operand1 = Double.parseDouble(dataFromFile[1]);
                 operand2 = Double.parseDouble(dataFromFile[2]);
             } catch (NumberFormatException e) {
-                System.out.println("Operand1 or Operand2 is incorrect \n" + e);
+                LOGGER.error("Operand1 or Operand2 is incorrect \n" + e);
             }
-
+//            bufferedReader.close();
         } catch (
                 IOException e) {
-            System.out.println("File not found " + e);
+            LOGGER.error("File not found " + e);
         }
     }
 
