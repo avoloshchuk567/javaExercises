b317b8f6e83b96974cf668dded9182a488527f9a
Alla Voloshchuk <avoloshchuk@griddynamics.com>
2020-03-12 13:40:38 +0200

    Corrections in FibonacciSequenceTest are provided.

--- src/test/java/fibonacciSequenceTests/FibonacciSequenceTest.java
+++ src/test/java/fibonacciSequenceTests/FibonacciSequenceTest.java
@@ -2,15 +2,9 @@ package fibonacciSequenceTests;
 
 import com.tngtech.java.junit.dataprovider.*;
 import fibonacciSequence.FibonacciSequenceDefinition;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.*;
-import org.junit.runner.RunWith;
-
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import org.junit.runner.*;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -20,30 +14,18 @@ public class FibonacciSequenceTest {
     FibonacciSequenceDefinition definition = new FibonacciSequenceDefinition();
 
     @DataProvider
-    public static Object[][] data(){
-        return new Object[][] {
-                {10, Arrays.asList(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)},
-                {1, Arrays.asList(1)},
-                {2, Arrays.asList(1, 1)}
+    public static Object[][] data() {
+        return new Object[][]{
+                {10, new Integer[]{1, 1, 2, 3, 5, 8, 13, 21, 34, 55}},
+                {1, new Integer[]{1}},
+                {2, new Integer[]{1, 1}},
         };
     }
 
     @Test
     @UseDataProvider("data")
-    public void sequenceOfTen(int number, List list) {
-        List actualList;
-        actualList = definition.defineFibonacciSequence(10);
-        assertThat(actualList, Matchers. hasItems(list));//containsAll(list));
-    }
-
-    @Test
-    public void sequenceOfOne() {
-        assertThat(definition.defineFibonacciSequence(1), contains(1));
-    }
-
-    @Test
-    public void sequenceOfTwo() {
-        assertThat(definition.defineFibonacciSequence(2), contains(1, 1));
+    public void sequenceOfTen(int number, Integer[] list) {
+        assertThat(definition.defineFibonacciSequence(number), Matchers.contains(list));
     }
 
     @Test
