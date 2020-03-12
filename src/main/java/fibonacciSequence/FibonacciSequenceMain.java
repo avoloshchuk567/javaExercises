967cbf568fb93f95f376e4178ea129f344e32294
Alla Voloshchuk <avoloshchuk@griddynamics.com>
2020-03-12 13:40:38 +0200

    Corrections in FibonacciSequenceTest are provided.

--- src/main/java/fibonacciSequence/FibonacciSequenceMain.java
+++ src/main/java/fibonacciSequence/FibonacciSequenceMain.java
@@ -8,8 +8,9 @@ public class FibonacciSequenceMain {
     private static final Logger LOGGER = LoggerFactory.getLogger(FibonacciSequenceMain.class);
 
     public static void main(String[] args) {
+        int to = 10;
         FibonacciSequenceDefinition definition = new FibonacciSequenceDefinition();
-        List<Integer> fibonacciSequence = definition.defineFibonacciSequence(15);
+        List<Integer> fibonacciSequence = definition.defineFibonacciSequence(to);
 
         for (int i : fibonacciSequence) {
             LOGGER.info(i + "");
---
+++ src/main/java/vampireNumbers/VampireNumbersMain.java
@@ -0,0 +1,7 @@
+package vampireNumbers;
+
+public class VampireNumbersMain {
+    public static void main(String[] args) {
+
+    }
+}
