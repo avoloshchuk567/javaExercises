0cbb6e308a0b0d731630b1bb2ca96cefeebbc01a
Alla Voloshchuk <avoloshchuk@griddynamics.com>
2020-03-19 14:26:55 +0200

    Tiny change in PrimeNumbersDefinition.

--- src/main/java/calculator/Calculation.java
+++ src/main/java/calculator/Calculation.java
@@ -2,23 +2,31 @@ package calculator;
 
 public class Calculation {
 
+    interface IntegerMath {
+        double operation(double operand1, double operand2);
+    }
+
+    public double operate(double operand1, double operand2, IntegerMath op) {
+        return op.operation(operand1, operand2);
+    }
+
     public Result calculateResult(char operation, double operand1, double operand2) {
         double result;
         switch (operation) {
             case '+': {
-                result = operand1 + operand2;
+                result = this.operate(operand1, operand2, (a, b) -> a + b);
                 break;
             }
             case '-': {
-                result = operand1 - operand2;
+                result = this.operate(operand1, operand2, (a, b) -> a - b);
                 break;
             }
             case '*': {
-                result = operand1 * operand2;
+                result = this.operate(operand1, operand2, (a, b) -> a * b);
                 break;
             }
             case '/': {
-                result = operand1 / operand2;
+                result = this.operate(operand1, operand2, (a, b) -> a / b);
                 break;
             }
             default: {
